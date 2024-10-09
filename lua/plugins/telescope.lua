916d5fb2a9a7f0b6f74e1f728200e8079f58b9c3
danizzvargas <danizzvargas@gmail.com>
2024-10-08 21:12:33 -0600

    Add telescope-ui-select.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,19 +1,35 @@
 -- Type: Navigation
--- Desc: Advanced file browser
+-- Desc: Advanced file browser and command navigation
 return {
-  'nvim-telescope/telescope.nvim',
-  keys = {
-    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
-    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+  {
+    'nvim-telescope/telescope.nvim',
+    keys = {
+      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+    },
+    init = function()
+      require("which-key").add({
+        { "<leader>f", group = "󰍉 Find" },
+      })
+    end,
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
   },
-  init = function()
-    require("which-key").add({
-      { "<leader>f", group = "󰍉 Find" },
-    })
-  end,
-  tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+          }
+        }
+      }
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
 
